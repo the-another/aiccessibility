87f2123747084c348dfac399c36caf0643e26452
Justin Kostka <128225253+jujoko7CF@users.noreply.github.com>
2025-03-15 18:49:26 +0100

    edit wordpress plugin headers

--- aiccessibility.php
+++ aiccessibility.php
@@ -4,8 +4,11 @@
  * Description: AI-powered accessibility improvements for WordPress content.
  * Version: poc
  * Plugin URI: https://github.com/the-another/aiccessibility
- * Plugin Author: CloudFest Hackathon 2025 Project Team
+ * Author: CloudFest Hackathon 2025 Project Team
  * Author URI: https://hackathon.cloudfest.com/project/aiccessiblity-content-updater/
+ * Requires PHP: 8.3
+ * Text Domain: aicu
+ * Domain Path: /lang
 */
 
 // If this file is called directly, abort.
